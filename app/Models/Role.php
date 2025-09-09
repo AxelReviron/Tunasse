98a748a397754dd5d36a60d6a6190e3f394609b7
AxelReviron <axel-reviron@proton.me>
2025-09-09 22:45:09 +0200

    Fix HasRole trait error

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Permission\Models\Role as SpatieRole;
-use Spatie\Permission\Traits\HasRoles;
 
 class Role extends SpatieRole
 {
-    use HasFactory, HasUuids, HasRoles;
+    use HasFactory, HasUuids;
 
     protected $primaryKey = 'uuid';
 }
