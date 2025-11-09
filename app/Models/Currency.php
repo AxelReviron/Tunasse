dc4217d789ccabec333f60d826bf5622233cd901
AxelReviron <axel-reviron@proton.me>
2025-11-09 15:06:56 +0100

    Currency (Model, Factory, Seeder, migration)

---
+++ app/Models/Currency.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Currency extends Model
+{
+    /** @use HasFactory<\Database\Factories\CurrencyFactory> */
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array<string>|bool
+     */
+    protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
+}
