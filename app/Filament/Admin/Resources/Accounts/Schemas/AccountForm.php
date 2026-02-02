fbe5a0cc14151c36d5ee976d154c5097c29b94a3
AxelReviron <axel-reviron@proton.me>
2026-02-02 22:24:48 +0100

    REF Add belowContent and set defaultCurrency

--- app/Filament/Admin/Resources/Accounts/Schemas/AccountForm.php
+++ app/Filament/Admin/Resources/Accounts/Schemas/AccountForm.php
@@ -3,16 +3,27 @@
 namespace App\Filament\Admin\Resources\Accounts\Schemas;
 
 use App\Enums\AccountType;
+use App\Models\Currency;
 use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Icon;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
 
 class AccountForm
 {
     public static function configure(Schema $schema): Schema
     {
+        $defaultCurrency = Currency::where('code', 'EUR')->first() ?
+            Currency::where('code', 'EUR')->first()->getKey() :
+            Currency::first() ?? new Currency([
+            'code' => 'EUR',
+            'name' => 'Euro',
+            'symbol' => '€',
+        ]);
+
         return $schema
             ->components([
                 Section::make()
@@ -28,16 +39,27 @@ class AccountForm
                             ->required(),
                         TextInput::make('balance')
                             ->label(__('account.balance'))
+                            ->belowContent([
+                                Icon::make(Heroicon::OutlinedQuestionMarkCircle),
+                                __('account.balance_helper')
+                            ])
+                            ->disabled()
                             ->required()
                             ->numeric()
                             ->default(0),
                         ColorPicker::make('color')
                             ->label(__('account.color'))
+                            ->belowContent([
+                                Icon::make(Heroicon::OutlinedQuestionMarkCircle),
+                                __('account.color_helper')
+                            ])
                             ->hexColor()
                             ->required(),
                         Select::make('currency_id')
                             ->label(__('currency.currency'))
-                            ->relationship('currency', 'name'),
+                            ->default($defaultCurrency)
+                            ->relationship('currency', 'name')
+                            ->required(),
                     ]),
             ]);
     }
