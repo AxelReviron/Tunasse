72eb38730966baf93e5babeb1cd4b374109fcec0
AxelReviron <axel-reviron@proton.me>
2026-04-26 23:53:12 +0200

    Make app PWA with Github Page

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,21 @@ import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
-    legacy()
+    legacy(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        start_url: '/',
+        scope: '/',
+      }
+    })
   ],
   resolve: {
     alias: {
